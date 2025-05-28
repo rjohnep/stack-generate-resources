49df50ecb3080678df4232cf9f315bdf1faaeaf0
Eugene Rogach <i.am.r.john@gmail.com>
2025-05-28 17:52:57 +0200

    add random password name

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "random_password" "this" {
   count = 1001 # Just adjust this number
   length = 9
 }
+
+resource "random_password" "name" {
+  count = 33 # Just adjust this number
+  length = 8
+}
