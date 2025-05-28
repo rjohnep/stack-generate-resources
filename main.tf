dd6c329c4431bb306699e0154d5829b6248d6ad8
Eugene Rogach <i.am.r.john@gmail.com>
2025-05-28 16:26:57 +0200

    add env resources

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "random_password" "this" {
   count = 1001 # Just adjust this number
   length = 9
 }
+
+resource ".env.local" "var" {
+  count = 33 # Just adjust this number
+  length = 9
+}
